5ce2045660cb1a487f1180a00d34a0862cf07160
hoangtv1899 <hoang.tran.1899@gmail.com>
2019-04-10 16:34:37 -0600

    Create bash file to run both subsetting and prioritizing flow

--- Topography/subset_elevation.sh
+++ Topography/subset_elevation.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-#require python3 and these libraries: gdal, ogr, osr, shapely, subprocess, numpy
+#require python3 and these libraries: gdal, ogr, osr, shapely, numpy
 # yum install wget
 # yum install openssl-devel
 # yum install libreadline-gplv2-dev libncursesw5-dev libssl-dev libsqlite3-dev tk-dev libgdbm-dev libc6-dev libbz2-dev

7d73eb87ee63597f9f183bf5b6dac0ff4c13ba2a
arezaii <ahmadrezaii@u.boisestate.edu>
2020-09-24 15:23:24 -0600

    update version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="parflow_subsetter",
-    version="1.0",
+    version="0.99.1",
     author="Ahmad Rezaii",
     author_email="ahmadrezaii@u.boisestate.edu",
     description="A set of tools for clipping ParFlow models and their outputs",

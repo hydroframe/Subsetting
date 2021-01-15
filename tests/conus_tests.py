840529f484c88e0b4e79679e9be45938bdb3014e
Ahmad Rezaii <ahmadrezaii@u.boisestate.edu>
2021-01-15 16:20:27 -0700

    disable test when src folder not found

--- tests/conus_tests.py
+++ tests/conus_tests.py
@@ -10,9 +10,12 @@ class ConusClassTests(unittest.TestCase):
     def test_normal_startup(self):
         if os.getenv('TRAVIS', default=False):
             pass
-        else:
+        elif os.path.isdir('../subset_1/CONUS1_inputs'):
             conus1 = Conus(local_path='../subset_1/CONUS1_inputs')
             self.assertEqual(conus1.manifest_file, data.conus_manifest)
+        else:
+            print('CONUS1_inputs not found, skipping test.')
+            pass
 
 
 if __name__ == '__main__':
